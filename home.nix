a2dac5972b3224900d6bd3d38c5b4117def89829
WillyDoc <guillermoreyno@institutoclaret.cl>
2026-09-01 11:53:01 -0400

    Instalar ecosistema de aplicaciones y unificar politicas de paquetes no libres

--- home.nix
+++ home.nix
@@ -4,11 +4,17 @@
 
   home.stateVersion = "24.11";
 
+
+  # Tus aplicaciones personales del día a día
   home.packages = [
-    pkgs.git
+    pkgs.git                        # Herramienta de control de versiones
+    pkgs.brave-origin               # Navegador web enfocado en privacidad
+    pkgs.keepassxc                  # Gestor de contraseñas local y seguro
+    pkgs.onlyoffice-desktopeditors   # Suite ofimática compatible con Office
+    pkgs.proton-authenticator       # Generador de códigos 2FA de Proton
   ];
 
-  # Configuración declarativa moderna de Git con tus datos oficiales
+  # Configuración declarativa moderna de Git
   programs.git = {
     enable = true;
     settings = {

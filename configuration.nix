203315452890ecd941112398ad9eba6f6e66b969
WillyDoc <guillermoreyno@institutoclaret.cl>
2026-09-02 09:57:18 -0400

    feat: agregar modulo flatpak y tienda bazaar

--- configuration.nix
+++ configuration.nix
@@ -94,4 +94,18 @@
 
   # Estado de compatibilidad del sistema
   system.stateVersion = "26.05";
+
+  # Configuración totalmente declarativa de Flatpaks y Tienda Bazaar
+  services.flatpak = {
+    enable = true;
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://flathub.org";
+      }
+    ];
+    packages = [
+      "io.github.kolunmi.Bazaar"
+    ];
+  };
 }
